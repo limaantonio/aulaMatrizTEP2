0d9d4a9b3c6af11bb3ffe23a27552b192a5c08af
antonio <limaantoniocarlos20@gmail.com>
2019-05-27 21:49:35 -0300

    Aula de TEP exercicio 2 de matrizes

---
+++ src/application/Program.java
@@ -0,0 +1,56 @@
+package application;
+
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		int n, m, x;
+
+		Scanner sc = new Scanner(System.in);
+		m = sc.nextInt();
+		n = sc.nextInt();
+
+		int mat[][] = new int[m][n];
+
+		for (int i = 0; i < m; i++) {
+			for (int j = 0; j < n; j++) {
+				mat[i][j] = sc.nextInt();
+			}
+		}
+		for(int i=0; i<m; i++){
+		for(int j=0; j<n; j++){
+			System.out.printf("%d ",mat[i][j]);
+		}
+		System.out.println();
+		}
+		x = sc.nextInt();
+		for (int i = 0; i < m; i++) {
+			for (int j = 0; j < n; j++) {
+				if(x == mat[i][j]) {
+					System.out.printf("Position %d , %d:\n",i,j);
+						if(j != 0) {
+							System.out.printf("Left: %d\n",mat[i][j-1]);
+						}
+						if(i != 0) {
+							System.out.printf("Up: %d\n",mat[i-1][j]);
+						}
+						if(j != n-1) {
+							System.out.printf("Rigth: %d\n",mat[i][j+1]);
+						}
+						if(i != m-1) {
+							System.out.printf("Down: %d\n",mat[i+1][j]);
+						}
+				}
+			}
+		}
+		/*for (int i = 0; i < m; i++) {
+			for (int j = 0; j < n; j++) {
+				System.out.printf("%d ", mat[i][j]);
+			}
+			System.out.println();
+		}*/
+		
+	}
+}
