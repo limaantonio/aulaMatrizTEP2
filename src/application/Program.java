cb272aa8295ae278e8065ce5f10d8f684580e236
antonio <limaantoniocarlos20@gmail.com>
2019-05-27 22:02:17 -0300

    Aula de TEP exercicio 2 de matrizes removi o cometario

--- src/application/Program.java
+++ src/application/Program.java
@@ -45,12 +45,5 @@ public class Program {
 				}
 			}
 		}
-		/*for (int i = 0; i < m; i++) {
-			for (int j = 0; j < n; j++) {
-				System.out.printf("%d ", mat[i][j]);
-			}
-			System.out.println();
-		}*/
-		
 	}
 }
